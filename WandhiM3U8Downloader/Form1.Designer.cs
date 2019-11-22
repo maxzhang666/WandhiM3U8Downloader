f0823a917ed11471bb7253c4b39a9b32f15d9c7a
张志方 <373884384@qq.com>
2019-11-22 10:24:12 +0800

    添加项目文件。

---
+++ WandhiM3U8Downloader/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WandhiM3U8Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Init = new System.Windows.Forms.Button();
+            this.txt_Url = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btn_Init
+            // 
+            this.btn_Init.Location = new System.Drawing.Point(158, 12);
+            this.btn_Init.Name = "btn_Init";
+            this.btn_Init.Size = new System.Drawing.Size(75, 23);
+            this.btn_Init.TabIndex = 0;
+            this.btn_Init.Text = "解析";
+            this.btn_Init.UseVisualStyleBackColor = true;
+            // 
+            // txt_Url
+            // 
+            this.txt_Url.Location = new System.Drawing.Point(52, 12);
+            this.txt_Url.Name = "txt_Url";
+            this.txt_Url.Size = new System.Drawing.Size(100, 21);
+            this.txt_Url.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(73, 165);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 88);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txt_Url);
+            this.Controls.Add(this.btn_Init);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Init;
+        private System.Windows.Forms.TextBox txt_Url;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
